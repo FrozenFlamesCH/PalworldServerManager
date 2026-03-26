8f206a0caa312bbb576ae98427b77232508e056f
FrozenFlamesCH <59395172+FrozenFlamesCH@users.noreply.github.com>
2026-03-26 14:16:50 +0100

    Create IconDefinitions.h with FontAwesome icon constants

---
+++ src/ui/IconDefinitions.h
@@ -0,0 +1,13 @@
+// FontAwesome Icon Constants
+
+#ifndef ICON_DEFINITIONS_H
+#define ICON_DEFINITIONS_H
+
+// Icon constants for FontAwesome icons
+#define ICON_HOME "\uf015"
+#define ICON_USER "\uf007"
+#define ICON_SETTINGS "\uf013"
+#define ICON_INFO "\uf129"
+#define ICON_PHONE "\uf095"
+
+#endif // ICON_DEFINITIONS_H
